2b0fec997308cb12991c36d1e80f8db91a60fdc1
lesg1151860 <luiseduardosg@ufps.edu.co>
2025-06-08 18:23:22 -0500

    Refactor administración de pedidos y limpieza de código

--- project/sweetsky/admin.py
+++ project/sweetsky/admin.py
@@ -1,25 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-
 from .models import Sauce, Topping, Product, Presentation, Order
 
-# Registrar modelos básicos
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'presentation', 'total_price', 'status', 'creation_date')
+    list_filter = ('status', 'city', 'delivery')
+    search_fields = ('client__username', 'delivery_address')
+    date_hierarchy = 'creation_date'
+    readonly_fields = ('total_price', 'creation_date')
+
 admin.site.register(Sauce)
 admin.site.register(Topping)
 admin.site.register(Product)
-admin.site.register(Order)
-
-# Si deseas personalizar el admin de User, descomenta lo siguiente:
-# from django.contrib.auth.admin import UserAdmin
-# admin.site.unregister(User)
-# @admin.register(User)
-# class CustomUserAdmin(UserAdmin):
-#     list_display = ('username', 'email', 'is_staff', 'is_active')
-
-@admin.register(Presentation)
-class PresentationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'product', 'price', 'status')
-    list_filter = ('product', 'status')
-    search_fields = ('name', 'product__name')
-    list_editable = ('status',)
-    ordering = ('-creation_date',)
+admin.site.register(Presentation)
